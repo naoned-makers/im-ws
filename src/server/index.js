a254aafe1379cb61f97d21bc3afd4fc2e9166078
Franck Wlodarezack <ext.sqli.franck.wlodarezack@sncf.fr>
2017-09-08 14:19:15 +0200

    Refactor : using socket.io with express
    Observe code standards according to documentation (https://socket.io/docs/#using-with-express-3/4)

--- src/server/index.js
+++ src/server/index.js
@@ -9,18 +9,18 @@ import * as movement from '../common/movementNames';
 dotenv.config();
 
 const app = express();
-const server = http.createServer(app);
-const io = socket.listen(server);
+const server = http.Server(app);
+const io = socket(server);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   console.log('A client is coming...');
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 // Events set up after the connection
-io.sockets.on('connection', (socket) => {
+io.on('connection', (socket) => {
   console.log(`Client address : ${socket.request.connection.remoteAddress}`);
 
   socket.on(movement.MOVE_LEFT_ARM, (message) => pythonUtils.moveLeftArm(message));
